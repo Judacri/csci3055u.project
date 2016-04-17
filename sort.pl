085322e4937466c57459bbeef664d03e59fa043e
Judacribz <sheron.balas@gmail.com>
2016-04-17 18:24:24 -0400

    Update sort.pl

--- sort.pl
+++ sort.pl
@@ -5,21 +5,21 @@ use warnings;
 
 sub compare_switch
 {
-    my $list     = shift;
+    my $arr      = shift;
     my $a        = shift;
     my $b        = shift;
+
+    my @list     = @$arr;
     my $switched = 0;
-    print "$a           $b\n";
-print "@$list[$a] \> @$list[$b]\n";
-my @ret = (@$list[$a], @$list[$b]. $switched);
-    if (@$list[$a] > @$list[$b])
+    my @ret      = ($list[$a], $list[$b], $switched);
+
+    if ($list[$a] > $list[$b])
     {
-        $switched  = 1;
-        @ret = (@$list[$b], @$list[$a]. $switched);
-        print "switched";
+        $switched = 1;
+        @ret      = ($list[$b], $list[$a], $switched);
     }
 
-    return \@ret;
+    return @ret;
 }
 
 sub bubble_sort
@@ -28,8 +28,22 @@ sub bubble_sort
     my @list      = @$arr;
     my $listSize  = @list;
     my $notSorted = 0;
+    my $switched  = 0;
 
-    print "array @list with size $listSize\n";
+    for (my $i = 0; $i < ($listSize - 1); $i++)
+    {
+        ($list[$i], $list[$i+1], $switched) = compare_switch(\@list, $i, $i+1);
+            
+        $notSorted |= $switched;
 
+        if ($notSorted && $i == ($listSize - 2))
+        {
+            $i         = 0;
+            $notSorted = 0;
+        }
+    }
+
+    print " @list\n";
 }
+
 1;
